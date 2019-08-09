d4aa565bc6b690c7ff9c5eac896a12f37cea688a
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-09 19:40:50 +0430

    Initially added to rep

---
+++ src/main/java/com/gta/outage/OutageApplication.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package com.gta.outage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutageApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OutageApplication.class, args);
+    }
+
+}
