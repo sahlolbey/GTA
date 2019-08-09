b828fe05d938e0c00cd537126826109962670195
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-09 22:22:19 +0430

    Initially added

---
+++ src/main/js/result.js
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+import React from "react"
+
+
+class Result extends React.Component {
+
+  render() {
+
+    return (
+        <div>
+          <span>Subscriber added successfully</span>
+        </div>
+    )
+  }
+}
+
+
+export default Result;
+
+
+
