13143ce6219ac10f10c16606ca8439e122ed33fe
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-11 23:53:10 +0430

    converted field injection to constructor

--- src/main/java/com/gta/outage/service/SubscriberService.java
+++ src/main/java/com/gta/outage/service/SubscriberService.java
@@ -2,6 +2,7 @@ package com.gta.outage.service;
 
 import com.gta.outage.dao.SubscriberDAO;
 import com.gta.outage.model.Subscriber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,8 +10,12 @@ import java.util.List;
 
 @Service
 public class SubscriberService {
+    private final SubscriberDAO subscriberDAO;
 
-    private SubscriberDAO subscriberDAO;
+    @Autowired
+    public SubscriberService(SubscriberDAO subscriberDAO) {
+        this.subscriberDAO = subscriberDAO;
+    }
 
     public void addSubscriber(Subscriber subscriber) {
         try {
