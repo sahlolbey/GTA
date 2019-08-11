4c5151b0fbc9a0facc68830fad1358e56df7d734
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-12 00:01:17 +0430

    initially added

---
+++ src/test/java/com/gta/outage/service/SubscriberServiceTest.java
@@ -0,0 +1,14 @@
+package com.gta.outage.service;
+
+import org.junit.Test;
+
+public class SubscriberServiceTest {
+
+    @Test
+    public void addSubscriber() {
+    }
+
+    @Test
+    public void getAllSubscribers() {
+    }
+}
