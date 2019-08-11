80f2f65ed12ab4af9be91a14b5f234bda2e2e3b4
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-11 23:47:03 +0430

    initially added

---
+++ src/main/java/com/gta/outage/service/SubscriberService.java
@@ -0,0 +1,28 @@
+package com.gta.outage.service;
+
+import com.gta.outage.dao.SubscriberDAO;
+import com.gta.outage.model.Subscriber;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SubscriberService {
+
+    private SubscriberDAO subscriberDAO;
+
+    public void addSubscriber(Subscriber subscriber) {
+        try {
+            subscriberDAO.save(subscriber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Subscriber> getAllSubscribers() {
+        List<Subscriber> subscribers = new ArrayList<>();
+        subscriberDAO.findAll().forEach(subscribers::add);
+        return subscribers;
+    }
+}
