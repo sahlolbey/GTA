4b34e432df31ce0da30b16a01f0ce1f1e4b375b9
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-12 22:17:38 +0430

    connected to service layer and completed

--- src/main/java/com/gta/outage/rest/SignupAPIController.java
+++ src/main/java/com/gta/outage/rest/SignupAPIController.java
@@ -1,9 +1,12 @@
 package com.gta.outage.rest;
 
 import com.gta.outage.model.Subscriber;
+import com.gta.outage.service.SubscriberServiceImp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignupAPIController {
     private static final Logger logger = LoggerFactory.getLogger(SignupAPIController.class);
 
+    private final SubscriberServiceImp subscriberServiceImp;
+
+    @Autowired
+    public SignupAPIController(SubscriberServiceImp subscriberServiceImp) {
+        this.subscriberServiceImp = subscriberServiceImp;
+    }
+
     @RequestMapping("/api/addSubscriber")
-    public ResponseEntity<String> addSubscriber(Subscriber subscriber) {
+    public ResponseEntity<String> addSubscriber(@RequestBody Subscriber subscriber) {
         logger.info("addSubscriber called");
+        logger.info("name=" + subscriber.getName());
+        logger.info("email=" + subscriber.getEmail());
+        subscriberServiceImp.addSubscriber(subscriber);
         return ResponseEntity.ok().body("successfully created");
     }
 
