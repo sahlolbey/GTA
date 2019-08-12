0fc97418251afe86016b2cc8cc3c695368209cad
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-12 22:13:41 +0430

    initially added as a result of separating Implementation from declaration.

---
+++ src/main/java/com/gta/outage/service/SubscriberServiceImp.java
@@ -0,0 +1,35 @@
+package com.gta.outage.service;
+
+import com.gta.outage.dao.SubscriberDAO;
+import com.gta.outage.model.Subscriber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SubscriberServiceImp implements SubscriberService {
+
+
+    private SubscriberDAO subscriberDAO;
+
+    @Autowired
+    public SubscriberServiceImp(SubscriberDAO subscriberDAO) {
+        this.subscriberDAO = subscriberDAO;
+    }
+
+    public void addSubscriber(Subscriber subscriber) {
+        try {
+            subscriberDAO.save(subscriber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Subscriber> getAllSubscribers() {
+        List<Subscriber> subscribers = new ArrayList<>();
+        subscriberDAO.findAll().forEach(subscribers::add);
+        return subscribers;
+    }
+}
