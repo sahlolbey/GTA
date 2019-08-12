7b6aa8ad8bcfad0745813dde6dd14b15f688ac39
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-12 22:10:11 +0430

    initially added

---
+++ src/main/java/com/gta/outage/service/EmailServiceImp.java
@@ -0,0 +1,30 @@
+package com.gta.outage.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImp implements EmailService {
+    private JavaMailSender emailSender;
+
+    @Autowired
+    public EmailServiceImp(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    @Override
+    public void sendSimpleMessage(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            emailSender.send(message);
+        } catch (MailException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
