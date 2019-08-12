e5d56e1572fe4310f801c3740bb6500065da8cfd
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-12 22:15:00 +0430

    changed from class to interface

--- src/main/java/com/gta/outage/service/SubscriberService.java
+++ src/main/java/com/gta/outage/service/SubscriberService.java
@@ -1,33 +1,13 @@
 package com.gta.outage.service;
 
-import com.gta.outage.dao.SubscriberDAO;
 import com.gta.outage.model.Subscriber;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class SubscriberService {
-    private final SubscriberDAO subscriberDAO;
 
-    @Autowired
-    public SubscriberService(SubscriberDAO subscriberDAO) {
-        this.subscriberDAO = subscriberDAO;
-    }
+public interface SubscriberService {
 
-    public void addSubscriber(Subscriber subscriber) {
-        try {
-            subscriberDAO.save(subscriber);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    void addSubscriber(Subscriber subscriber);
 
-    public List<Subscriber> getAllSubscribers() {
-        List<Subscriber> subscribers = new ArrayList<>();
-        subscriberDAO.findAll().forEach(subscribers::add);
-        return subscribers;
-    }
+    List<Subscriber> getAllSubscribers();
 }
