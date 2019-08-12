bd35b6d4d19cfce4c1d1e1ede1029540b9babc6b
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-13 02:17:59 +0430

    some style changes

--- src/main/js/result.js
+++ src/main/js/result.js
@@ -1,8 +1,5 @@
-/**
- *
- */
-import React from "react"
-
+import React from "react";
+import "../resources/static/css/main.css";
 
 class Result extends React.Component {
 
@@ -10,10 +7,24 @@ class Result extends React.Component {
 
     return (
         <div>
-          <span>Subscriber added successfully</span>
+            <div style={{height: '50px'}}>&nbsp;</div>
+            <div className="center result" style={{width: '50%', height: '300px'}}>
+                <div className="form-group center" style={{width: 'max-content'}}>
+                    <span className="center" style={{marginTop: 20}}>Subscriber added successfully</span>
+                </div>
+                <div className="form-group center" style={{width: 'max-content'}}>
+                    <a onClick={() => this.handleClick()} className="btn btn-success center"
+                       style={{marginTop: 20}}>Add another </a>
+                </div>
+            </div>
         </div>
     )
   }
+    
+    handleClick() {
+        this.props.history.push("/");
+    }
+
 }
 
 
