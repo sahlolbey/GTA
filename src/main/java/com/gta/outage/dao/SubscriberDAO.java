61e1b22871dd223cd5410f2fd5ddd894c45b8b95
Hamid Sahlolbey <sahlolbey@gmail.com>
2019-08-15 01:12:56 +0430

    changed id type

--- src/main/java/com/gta/outage/dao/SubscriberDAO.java
+++ src/main/java/com/gta/outage/dao/SubscriberDAO.java
@@ -3,5 +3,5 @@ package com.gta.outage.dao;
 import com.gta.outage.model.Subscriber;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+public interface SubscriberDAO extends CrudRepository<Subscriber, String> {
 }
